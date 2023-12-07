5909041e8986d632b135932ff6a602ef8a67dcd0
ezgikardes <ezgikardes@gmail.com>
2023-12-07 17:14:17 +0300

    form validasyonu ve hata mesajları kısmı tamamlandı

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,156 +1,88 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import * as Yup from "yup" ;
-import { yupResolver } from "@hookform/resolvers/yup";
 
-export default function TaskHookForm({kisiler}) {
-  
-  const formSemasi = Yup.object().shape({
-    title: Yup.string()
-      .required("Task başlığı yazmalısınız")
-      .min(3, "Task başlığı en az 3 karakter olmalı"),
-    description: Yup.string()
-      .required("Task açıklaması yazmalısınız")
-      .min(5, "Task açıklaması en az 10 karakter olmalı"),
-    // people: Yup.array()
-    //   .max(3, "En fazla 3 kişi seçebilirsiniz")
-    //   .min(1, "Lütfen en az bir kişi seçin"),
-  });
-  
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors, isValid }
-  // } = useForm({resolver: yupResolver(formSemasi) });
-
-  const TaskForm = ({kisiler, submitFn}) => {
-    const {
-      register,
-      handleSubmit,
-      formState: { errors, isValid }
-    } = useForm({resolver: yupResolver(formSemasi) });
-    }
-  
+export default function TaskHookForm({ kisiler }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: "onChange" });
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <p>
-        <label>
-          İsim: &nbsp;
-          
-          <input
-            className="input-text"
-            type="text"
-            name="title"
-            {...register("title", { required: "title is required" })}
-          />
-          {errors.title && <span>{errors.title.message}</span>}
+    <form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Başlık
+          <input 
+            className="input-text" 
+            type="text" 
+            name="title" 
+            {...register("title", {
+              required: "Task başlığı yazmalısınız", 
+              minLength: {
+                value: 3,
+                message: "Task başlığı en az 3 karakter olmalı"
+              }
+            })}/>
+            {errors.title && <p>{errors.title.message}</p>}
         </label>
-      </p>
-      <br />
-      <p>
-        <label>
-          Görev: &nbsp;
-          <input
+      </div>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+          <textarea
             className="input-textarea"
+            rows="3"
             type="text"
             name="description"
             {...register("description", {
-              required: "description is required",
+              required: "Task açıklaması yazmalısınız",
+              minLength: {
+                value: 10, 
+                message: "Task açıklaması en az 10 karakter olmalı"
+              }
             })}
           />
-          {errors.description && <span>{errors.description.message}</span>}
-          
+          {errors.description && <p>{errors.description.message}</p>}
         </label>
-      </p>
-      <br />
-      <p>
-      {kisiler.map((p) => (
-            <label className="input-checkbox" key={p}>
-              <input
-                type="checkbox"
-                name="people"
-                value={p}
-              />
-              {p}
-            </label>
-          ))}
-      </p>
+      </div>
 
-      <br />
-      <input type="submit" disabled={!isValid}/>
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        {kisiler.map((e) => (
+          <label className="input-checkbox" key={e}>
+            <input 
+            type="checkbox" 
+            name="people"
+            value={e}
+            {...register("people", {
+              maxLength: {
+                value: 3,
+                message: "En fazla 3 kişi seçebilirsiniz"
+              }, 
+              minLength: {
+                value: 1, 
+                message: "Lütfen en az bir kişi seçin"
+              }
+            })} 
+            />
+            {e}
+          </label>
+        ))}
+        {errors.people && <p>{errors.people.message}</p>}
+      </div>
+
+      <div className="form-line">
+        <button className="submit-button" type="submit" disabled={!isValid}>
+          Kaydet
+        </button>
+      </div>
     </form>
   );
 }
-
-
-// chat gpt:
-
-// import React from "react";
-// import { useForm } from "react-hook-form";
-// import * as Yup from "yup";
-// import { yupResolver } from "@hookform/resolvers/yup";
-
-// export default function TaskHookForm({ kisiler }) {
-//   const formSemasi = Yup.object().shape({
-//     title: Yup.string()
-//       .required("Task başlığı yazmalısınız")
-//       .min(3, "Task başlığı en az 3 karakter olmalı"),
-//     description: Yup.string()
-//       .required("Task açıklaması yazmalısınız")
-//       .min(5, "Task açıklaması en az 5 karakter olmalı"),
-//   });
-
-//   const { register, handleSubmit, formState: { errors, isValid } } = useForm({
-//     resolver: yupResolver(formSemasi),
-//   });
-
-//   const onSubmit = (data) => {
-//     console.log(data);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <p>
-//         <label>
-//           İsim: &nbsp;
-//           <input
-//             className="input-text"
-//             type="text"
-//             name="title"
-//             {...register("title")}
-//           />
-//           {errors.title && <span>{errors.title.message}</span>}
-//         </label>
-//       </p>
-//       <br />
-//       <p>
-//         <label>
-//           Görev: &nbsp;
-//           <input
-//             className="input-textarea"
-//             type="text"
-//             name="description"
-//             {...register("description")}
-//           />
-//           {errors.description && <span>{errors.description.message}</span>}
-//         </label>
-//       </p>
-//       <br />
-//       <p>
-//         {kisiler.map((p) => (
-//           <label className="input-checkbox" key={p}>
-//             <input type="checkbox" name="people" value={p} />
-//             {p}
-//           </label>
-//         ))}
-//       </p>
-//       <br />
-//       <input type="submit" disabled={!isValid} />
-//     </form>
-//   );
-// }
